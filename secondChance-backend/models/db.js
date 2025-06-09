c1f74cab694aea762902823371fd69539dec9e3a
SametDulger <sametd94@gmail.com>
2025-06-09 15:47:32 -0400

    .

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -1,28 +1,28 @@
 // db.js
 require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
-
-// MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
+const { MongoClient } = require('mongodb');
 
 let dbInstance = null;
-const dbName = `${process.env.MONGO_DB}`;
+
+// MongoDB connection URI and database name from environment variables
+const url = process.env.MONGO_URL;
+const dbName = process.env.MONGO_DB;
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+    // Return existing database instance if already connected
+    if (dbInstance) {
+        return dbInstance;
+    }
 
-    const client = new MongoClient(url);      
+    // Task 2: Connect to MongoDB
+    const client = new MongoClient(url);
+    await client.connect();
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+    // Task 3: Connect to the database and store it in dbInstance
+    dbInstance = client.db(dbName);
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+    // Task 4: Return the database instance
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
